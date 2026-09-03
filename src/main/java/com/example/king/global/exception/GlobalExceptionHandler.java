a94502ea780c45f40fae93377bf107970160b5a2
yunbom-sudo <yunbom100929@gmail.com>
2026-09-03 21:22:44 +0900

    remove: global exception 폴더 삭제

--- src/main/java/com/example/king/global/exception/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.king.global.exception;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-}
